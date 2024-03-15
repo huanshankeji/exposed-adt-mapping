1c1c842ebf15c8a9de397386399a8d641e636f64
Shreck Ye <ShreckYe@gmail.com>
2024-03-15 10:13:25 +0800

    v0.1.0 release

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -1,7 +1,7 @@
 import com.huanshankeji.CommonDependencies
 import com.huanshankeji.CommonVersions
 
-val projectVersion = "0.1.0-SNAPSHOT"
+val projectVersion = "0.1.0"
 
 val commonVersions = CommonVersions()
 val commonDependencies = CommonDependencies(commonVersions)
