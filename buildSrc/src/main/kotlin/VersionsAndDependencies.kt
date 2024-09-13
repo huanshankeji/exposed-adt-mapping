8c931425f3df8f9880f5b662f3225c9e9be3db3e
Shreck Ye <ShreckYe@gmail.com>
2024-09-13 22:33:18 +0800

    Update the project version to "0.2.0-SNAPSHOT"

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -1,7 +1,7 @@
 import com.huanshankeji.CommonDependencies
 import com.huanshankeji.CommonVersions
 
-val projectVersion = "0.1.0-SNAPSHOT"
+val projectVersion = "0.2.0-SNAPSHOT"
 
 val commonVersions = CommonVersions()
 val commonDependencies = CommonDependencies(commonVersions)
