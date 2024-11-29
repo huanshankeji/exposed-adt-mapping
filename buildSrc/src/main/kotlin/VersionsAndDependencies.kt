57e5f95e12d71ebace54c55cecce2b3f0a3ab326
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-11-29 21:39:56 +0800

    Bump "kotlin-common" to v0.6.0

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -3,5 +3,5 @@ import com.huanshankeji.CommonVersions
 
 val projectVersion = "0.3.0-SNAPSHOT"
 
-val commonVersions = CommonVersions(kotlinCommon = "0.5.1", exposed = "0.56.0")
+val commonVersions = CommonVersions(kotlinCommon = "0.6.0", exposed = "0.56.0")
 val commonDependencies = CommonDependencies(commonVersions)
