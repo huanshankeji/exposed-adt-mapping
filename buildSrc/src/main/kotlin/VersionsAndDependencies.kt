4260bb891fdb047e89ad4c7752eb379400bfb068
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-01 17:15:37 +0800

    Bump the project version to 0.3.1-SNAPSHOT

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -1,7 +1,7 @@
 import com.huanshankeji.CommonDependencies
 import com.huanshankeji.CommonVersions
 
-val projectVersion = "0.3.0-SNAPSHOT"
+val projectVersion = "0.3.1-SNAPSHOT"
 
 val commonVersions = CommonVersions(kotlinCommon = "0.6.0", exposed = "0.56.0")
 val commonDependencies = CommonDependencies(commonVersions)
