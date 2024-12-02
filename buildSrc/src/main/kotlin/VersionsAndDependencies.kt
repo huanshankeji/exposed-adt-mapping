a2527db5ef2fc860366d3b6bfeccdb0c7cb656f4
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-02 17:49:10 +0800

    Make the project version explicit

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -1,7 +1,7 @@
 import com.huanshankeji.CommonDependencies
 import com.huanshankeji.CommonVersions
 
-val projectVersion = "0.3.1-SNAPSHOT"
+val projectVersion = "0.3.1-kotlin-2.1.0-SNAPSHOT"
 
 val commonVersions = CommonVersions(kotlinCommon = "0.6.0", exposed = "0.56.0")
 val commonDependencies = CommonDependencies(commonVersions)
