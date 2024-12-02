9bac924a2b900df99a6e1bd28f6ae099fd50ffd0
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-02 18:33:14 +0800

    Bump our dependencies the latest snapshots using Kotlin 2.1.0

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -3,5 +3,6 @@ import com.huanshankeji.CommonVersions
 
 val projectVersion = "0.3.1-kotlin-2.1.0-SNAPSHOT"
 
-val commonVersions = CommonVersions(kotlinCommon = "0.6.0", exposed = "0.56.0")
+// TODO don't use a snapshot version in a main branch
+val commonVersions = CommonVersions(kotlinCommon = "0.6.1-kotlin-2.1.0-SNAPSHOT", exposed = "0.56.0")
 val commonDependencies = CommonDependencies(commonVersions)
