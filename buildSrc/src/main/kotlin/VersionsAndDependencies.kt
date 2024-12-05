358a2d4f0444f7c1c88f2cadd572ac464c4b148b
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-05 17:36:12 +0800

    Bump kotlin-common to v0.6.1 and remove the explicit Exposed version

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -3,6 +3,6 @@ import com.huanshankeji.CommonVersions
 
 val projectVersion = "0.3.1-SNAPSHOT"
 
-// TODO don't use a snapshot version in a main branch
-val commonVersions = CommonVersions(kotlinCommon = "0.6.1-SNAPSHOT", exposed = "0.56.0")
+// don't use a snapshot version in a main branch
+val commonVersions = CommonVersions(kotlinCommon = "0.6.1")
 val commonDependencies = CommonDependencies(commonVersions)
